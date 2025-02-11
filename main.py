ab300757b9861d652209b30a31ec61e3dc82160c
tamile oliveira <tamile.oliveira@hotmail.com>
2025-02-11 20:14:52 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+
+while True:
+    # check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()  # close window
+            quit()  # end pygame
