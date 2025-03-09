82de8d639b7947f9868e7b89392135971712d233
tamile oliveira <tamile.oliveira@hotmail.com>
2025-03-09 08:35:50 -0300

    SQlite3 implementation for Score using proxy design pattern

--- code/Const.py
+++ code/Const.py
@@ -1,9 +1,9 @@
 # C
 import pygame
 
-C_ORANGE = (255, 120, 0)
-C_WHITE = (255, 255, 255)
+C_ORANGE = (255, 128, 0)
 C_YELLOW = (255, 255, 128)
+C_WHITE = (255, 255, 255)
 C_GREEN = (0, 128, 0)
 C_CYAN = (0, 128, 128)
 
@@ -112,7 +112,7 @@ ENTITY_SHOT_DELAY = {
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
-               'NEW GAME 2 P - COMPETITIVE',
+               'NEW GAME 2P - COMPETITIVE',
                'SCORE',
                'EXIT')
 
@@ -134,7 +134,24 @@ SPAWN_TIME = 4000
 # T
 TIMEOUT_STEP = 100  # 100ms
 TIMEOUT_LEVEL = 20000  # 20s
-
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+# S
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'EnterName': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+
+}
--- code/game.py
+++ code/game.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import pygame
-from pygame.examples.aliens import Score
 
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
+from code.Score import Score
 from code.level import Level
 from code.menu import Menu
 
@@ -15,7 +15,7 @@ class Game:
 
     def run(self):
         while True:
-            # score = Score(self.window)
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -26,9 +26,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
-                    # if level_return:
-                    # score.save(menu_return,player_score)
+                if level_return:
+                    score.save(menu_return,player_score)
 
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()  # end pygame
